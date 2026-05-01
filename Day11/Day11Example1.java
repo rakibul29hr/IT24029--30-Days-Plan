1602b070a9e2ecae3a334c84d5591ae410c9c160
Rakibul Islam <it24029@mbstu.ac.bd>
2026-05-02 00:24:41 +0600

    Create Day11Example1.java

---
+++ Day11/Day11Example1.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Day11Example1 {
+
+    public static void main(String[] args)
+    {
+        LocalDate today= LocalDate.now();
+        System.out.println("Today's date: "+ today);
+    }
+
+}
